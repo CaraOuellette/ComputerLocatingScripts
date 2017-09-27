3341fac771230127455c45bc1e69c81f70c7910c
Cara Ouellette <jouellet@apotex.com>
2017-09-27 14:06:38 -0400

    Extending location to 1.match database format 2.improve indexing speed for locations 3.introduce structured relationship for   different levels of locational organization.

--- LocationAssociation/Location.py
+++ LocationAssociation/Location.py
@@ -1,41 +1,51 @@
 class Location:
-    """A place in the world where Apotex employees may have been given a computer.
+    """A place in the world where there may have been given a computer.
+    
+    DOMAIN -- a dictionary mapping Domain names to Regions
+    REGION -- a dictionary mapping Region names to Site names
+    SITE -- a dictionary mapping Site names to specific Location objects
+    format_prefixes --Changes a string into a list of strings.
+    format_IP -- A method that changes a string in a list of integers.
+    format_dicts -  Creates a "tree" (linked dictionaries) of location levels.
     """
-    # A dictionary mapping Location names to the number of duplicate names
-    instances = {}
+    
+    DOMAIN = {}
+    # a dictionary mapping Domain names to Regions
+    REGION = {}
+    # a dictionary mapping Region names to Site names
+    SITE = {}
+    # a dictionary mapping Site names to specific Location objects
 
-    def __init__(self, name, prefixes, IP):
+    def __init__(self, domain, region, site, name = "", prefixes = [], IP = ""):
         """Creates a new instance of the Location class.
 
-        name: a String representing a real-world location
-        prefixes: a list of strings corresponding to naming conventionS.
-        IP: a list of 4 integers corresponding to an IP address's four octets.
-        If IPv6 used, list is empty.
+        domain -- the highest level of locational organization
+        region -- a high level of locational organization
+        site -- the basic level of locational organization
+        name -- a more specific location among sites
+        prefixes -- A String corresponding to computer naming conventions.
+        IP - a String corresponding to an IP address. Not IPv6 sensitive.
 
         Returns a Location object."""
+        self.domain = domain
+        self.region = region
+        self. site = site
         self.name = name
-        self.prefixes = self.formatPrefixes(prefixes)
-        if len(prefixes) < 1:
-            self.hasPrefixes = False
-        else:
-            self.hasPrefixes = True
-        self.IP = self.formatIP(IP)
-        if self.name in Location.instances.keys():
-            self.instance = len(Location.instances[self.name]) + 1
-            Location.instances[self.name].append(self)
-        else:
-            self.instance = 1
-            Location.instances[self.name] = [self]
+        self.prefixes = self.format_prefixes(prefixes)
+        self.IP = self.format_IP(IP)
+        #If this location has other representations, document that
+        #possible antiquated, based on mistaken conception of IP checking
+        self.format_dicts(domain, region, site)
 
-    def formatPrefixes(self, prefixString):
-        """breaks up a string of prefixes into a list separated by commas"""
+    def format_prefixes(self, prefixString):
+        """Breaks up a string of prefixes into a list, comma delimited."""
         if not prefixString == "":
             lst = prefixString.split(",")
             return lst
         return []
 
-    def formatIP(self, IP):
-        """breaks up a string of an IP into a list of 4 integers"""
+    def format_IP(self, IP):
+        """Breaks up a string of an IP into a list of 4 integers, period delimited."""
         if "." in IP:
             finIP = IP.split(".")
             if len(finIP) < 4:
@@ -49,3 +59,29 @@ class Location:
                 return realIP
         else:
             return [0, 0, 0, 0]
+
+        def format_dicts(self, domain, region, site):
+            """Creates a "tree" (linked dictionaries) to help index locations.
+            
+            First the location object is added to the SITE dictionary. If no
+            key exists for the given site, then one is created. Next, the
+            REGION dictionary is checked for the region parameter. If it is 
+            present, then that list is checked for the site parameter. If this
+            is not present, then it is added. If the region is not present, it
+            is added along with the site as a one item list. The same step is 
+            then repeated at the domain level.
+            """
+            if site in location.SITE:
+                location.SITE[site].append(self)
+            else:
+                location.SITE[site] = [self]
+            if region in location.REGION:
+                if site not in location.REGION[region]:
+                    location.REGION[region].append(site)
+            else:
+                location.REGION[region] = [site]
+            if domain in location.DOMAIN:
+                if region not in location.DOMAIN[domain]:
+                    location.DOMAIN[domain].append(region)
+            else:
+                location.DOMAIN[domain] = [region]
